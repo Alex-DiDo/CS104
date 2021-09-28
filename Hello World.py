b9e72c3ecea44341473422127f50944b3f05d3b7
Alex-DiDo <91150616+Alex-DiDo@users.noreply.github.com>
2021-09-27 21:30:09 -0400

    Update Hello World.py

--- Hello World.py
+++ Hello World.py
@@ -3,4 +3,3 @@ Python 3.9.7 (v3.9.7:1016ef3790, Aug 30 2021, 16:39:15)
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> print("Hello my name is Alex")
 Hello my name is Alex
->>> 
