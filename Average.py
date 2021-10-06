186d0c4e3016de4ef6987f5f6d1b68f266e26b89
Alex-DiDo <91150616+Alex-DiDo@users.noreply.github.com>
2021-10-06 10:40:50 -0400

    Update and rename Hello World Version 2.py to Average.py

---
+++ Average.py
@@ -0,0 +1,11 @@
+average = 0
+total = 0
+howManyEntered = 0
+howMany = int(input('How many test scores would you like to average\n'))
+while howMantEntered < howMany:
+  test score = int(input('Enter test score:\n'))
+  total += testscore
+  howManyEntered += 1 
+  average = total / howMany 
+ print('The average for the test scores you enter is:')
+print(average) 
--- Hello World Version 2.py
+++
@@ -1,8 +0,0 @@
-Python 3.9.7 (v3.9.7:1016ef3790, Aug 30 2021, 16:39:15) 
-[Clang 6.0 (clang-600.0.57)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
->>> print("Hello my name is Alex")
-Hello my name is Alex
->>> # This is Version 2 of my Hello World Program
->>> print("Hello my name is Alex DiDomenico")
-Hello my name is Alex DiDomenico
